e54a35c15f05549a9dc65c0d3e6a45e11668df77
lawliet <aniquekhawar2021@u.northwestern.edu>
2022-08-11 20:59:34 -0400

    Fixed column naming conventions

--- download_jynneos_table.py
+++ download_jynneos_table.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
-today = pd.Timestamp.now(tz='US/Eastern').strftime('%Y-%m-%d')
+# grab today
+today_timestamp = pd.Timestamp.now(tz='US/Eastern')
+today_str = today_timestamp.strftime('%Y-%m-%d')
+last_updated_time = today_timestamp.strftime('%Y-%m-%d %H:%M')
+
+# get data
 url = 'https://aspr.hhs.gov/SNS/Pages/JYNNEOS-Distribution.aspx'
 df = pd.read_html(url)[0]
-replace_dict = {'Total Requested': 'Total Requested '}
-df.columns = df.columns.str.replace('Total Requested', 'Total Requested ')
-df.to_csv(f'data/JYNNEOS_vaccine_distribution_{today}.csv', index = False)
+
+# rename columns
+replace_tuple = [('Total Requestedas', 'Total Requested as'), ('Total Shippedas', 'Total Shipped as')]
+for old_text, new_text in replace_tuple:
+    df.columns = df.columns.str.replace(old_text, new_text)
+
+# update last updated field
+df['Last Updated'] = last_updated_time
+
+# save to CSV
+df.to_csv(f'data/JYNNEOS_vaccine_distribution_{today_str}.csv', index = False)
